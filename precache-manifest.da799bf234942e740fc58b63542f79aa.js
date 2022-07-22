db679ff5b11abde202f960073eac5951ae2597b3
HH-pro <77552469+HH-pro@users.noreply.github.com>
2022-07-22 16:49:33 +0500

    Updates

---
+++ precache-manifest.da799bf234942e740fc58b63542f79aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b44ddc1be93f719e895",
+    "url": "/static/css/main.6b9802a4.chunk.css"
+  },
+  {
+    "revision": "3b44ddc1be93f719e895",
+    "url": "/static/js/main.3b44ddc1.chunk.js"
+  },
+  {
+    "revision": "73d3c09f02479628f9ef",
+    "url": "/static/js/1.73d3c09f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4695890b538956f35a67d579e30e9fa3",
+    "url": "/index.html"
+  }
+];
